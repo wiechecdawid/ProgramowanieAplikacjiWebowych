9cd19b7828c8fd5a942080cf5f63a4dbd966976e
wiechecdawid <wiechec.dawid@gmail.com>
2021-03-29 00:59:10 +0200

    DrumBox skeleton

--- Projekt2/script.ts
+++ Projekt2/script.ts
@@ -1,13 +1,53 @@
-document.addEventListener('keypress', onKeyPressed)
+let clapSound: HTMLAudioElement;
+let kickSound: HTMLAudioElement;
+let channel1Btn: HTMLButtonElement;
+
+const channelArr: any[] = [];
+
+function appStart(): void {
+    document.addEventListener('keypress', onKeyPressed);
+    
+    channel1Btn = document.querySelectorAll('.channelButton')[0] as HTMLButtonElement;
+    channel1Btn.addEventListener('click', onChannelPlayed);
+
+    getAudioTags();
+}
+
+function onChannelPlayed(): void {
+    channelArr.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time);
+    })
+}
+
+function getAudioTags(): void {
+    // const clapSound = document.querySelector('#clap');
+    clapSound = document.querySelector('[data-sound="clap"]');
+    kickSound = document.querySelector('[data-sound="kick"]');
+}
 
 function onKeyPressed(ev: KeyboardEvent): void {
     const key = ev.key;
     const stamp = ev.timeStamp;
 
-    // const clapSound = document.querySelector('#clap');
-    const clapSound: HTMLAudioElement = document.querySelector('[data-sound="clap"]');
-    clapSound.currentTime = 0;
-    clapSound.play();
+    channelArr.push({key, stamp});
+
+    playSound(key);
 
     console.log(ev);
 }
+
+
+function playSound(key: string): void {
+    switch(key) {
+        case 'q':
+            clapSound.currentTime = 0;
+            clapSound.play();
+            break;
+        case 'w':
+            kickSound.currentTime = 0;
+            kickSound.play();
+            break;
+    }
+}
+
+appStart();
